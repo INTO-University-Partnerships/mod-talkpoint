819e0c35798a635ecb092dce571fe23ffa40628e
Mike McGowan <mike.mcgowan@intoglobal.com>
2016-07-29 15:58:48 +0100

    Added missing $plugin->component from version.php that Moodle 3.x wants

--- version.php
+++ version.php
@@ -2,6 +2,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2015070900;
-$module->requires  = 2013051402;
-$module->component = 'mod_talkpoint';
+$plugin->version   = 2015070900;
+$plugin->requires  = 2013051402;
+$plugin->component = 'mod_talkpoint';
